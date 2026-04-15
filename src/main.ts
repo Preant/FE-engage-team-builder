9ce2a0045f0de51b95ceac41345cc74e5c302c17
Paolo <preant-hamadi@oxyl.fr>
2026-04-15 11:30:26 +0200

    chore: migrate to Angular 21
    - Update all @angular packages from 19.x to 21.x
    - Update @angular/cdk and @angular/material from 18.x to 21.x
    - Update TypeScript from 5.6.x to 5.9.x (required for Angular 21)
    - Update @analogjs packages from 1.x to 2.x
    - Update PrimeNG from 19.0.0-rc.1 to 21.x (stable)
    - Update @primeng/themes from 18.x to 21.x
    - Remove NgModule AppRoutingModule and use standalone provideRouter()
    - Create app.routes.ts for route definitions
    - Update tsconfig.json moduleResolution from node to bundler
    - Update angular.json build budgets for new bundle size

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import Aura from '@primeng/themes/aura';
 import { ConfirmationService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 
-import { AppRoutingModule } from './app/app-routing.module';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 import { appInitializer } from '@/app/config/appInitializer';
@@ -20,7 +20,7 @@ bootstrapApplication(AppComponent, {
         preset: Aura
       }
     }),
-    importProvidersFrom(AppRoutingModule),
+    provideRouter(routes),
     ConfirmationService,
     appInitializer
   ]
